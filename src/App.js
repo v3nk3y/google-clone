9a72d3e522e5f96eba921c71ce5ac14f3ca5e944
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-15 17:49:23 -0400

    home page base header and google image body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Google Clone</h1>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the Search Page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
