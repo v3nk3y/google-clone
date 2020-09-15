071194cfc0118104f9da36bf1d054cfacb1b8a4b
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-15 18:28:54 -0400

    Search input with buttons added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from "../components/Search";
 
 function Home() {
   return (
@@ -24,7 +25,9 @@ function Home() {
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt=""
         />
-        <div className="home__inputContainer"></div>
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   );
